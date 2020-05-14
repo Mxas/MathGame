9792ca5bfb0bc037c75ea9f34c689c3515300ab0
Mindaugas Karpinskas <mkarpinskas@exadel.com>
2020-05-14 21:42:19 +0300

    some small commit

--- math-game/src/main/java/lt/mk/mathgame/pane/AnswerDialog.java
+++ math-game/src/main/java/lt/mk/mathgame/pane/AnswerDialog.java
@@ -1,6 +1,5 @@
 package lt.mk.mathgame.pane;
 
-import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
@@ -8,7 +7,6 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -51,10 +49,9 @@ public class AnswerDialog extends Dialog<String> {
 
         headerLabel.setFont(Font.font("Arial", FontWeight.BOLD, 24));
         bottom.getChildren().add(headerLabel);
-        GridPane.setHalignment(headerLabel, HPos.CENTER);
 
 
         getDialogPane().setContent(container);
-        getDialogPane().getButtonTypes().setAll(ButtonType.OK);
+        getDialogPane().getButtonTypes().setAll(ButtonType.YES);
     }
 }
